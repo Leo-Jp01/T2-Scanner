bf8e48ba7fb370f6a4b8d1e2a26562a6ff2d36f5
LeoJaraba <leo.jaraba.dev@gmail.com>
2025-09-29 21:41:42 -0500

    refactor(update): correction in docstring

--- my_validators/file_validator.py
+++ my_validators/file_validator.py
@@ -3,14 +3,14 @@ from pathlib import Path
 
 #---------------- File Validator ------------------
 def is_valid_file(file_user: str) -> bool:
-    """Funcion that take the path of a file and return True if the file exists and this is 
+    """Funcion that take the path of a file and return True if file exists and this is 
        not empty in other case return False
 
     Args:
         file_user (str): Take the path of a file in str format
 
     Returns:
-        bool: True if the file exists and this is not empty, False in other case.
+        bool: True if file exists and this is not empty, False in other case.
     """
     
     try:
